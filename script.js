aac1abb93d15925e3e9524cad44c363367edb729
PJ Jones <pj.jones4000.com>
2022-02-07 17:14:46 -0600

    will only log this message :Accusation 101:I accuse friend2, with the weapon2 in the location2. something wrong with the closure obviously

---
+++ script.js
@@ -0,0 +1,59 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var div = document.createElement("div");
+  document.body.appendChild(div);
+
+  function run() {
+    console.log(
+      `Accusation ${i}:I accuse ${friends[i % friends.length]}, with the ${
+        weapons[i % weapons.length]
+      } in the ${locations[i % locations.length]}.`
+    );
+  };
+
+  for (var i = 1; i <= 100; i++) {
+    var h3 = document.createElement("h3");
+    h3.addEventListener("click", run);
+    h3.textContent = "Accusation " + i;
+    h3.id = "acc" + i;
+
+    
+
+    div.appendChild(h3);
+  }
+
+  var friends = ["friend1", "friend2", "friend3", "friend4", "friend5"];
+  var locations = [
+    "location1",
+    "location2",
+    "location3",
+    "location4",
+    "location5",
+    "location6",
+    "location7",
+    "location8",
+    "location9",
+    "location10",
+  ];
+  var weapons = [
+    "weapon1",
+    "weapon2",
+    "weapon3",
+    "weapon4",
+    "weapon5",
+    "weapon6",
+    "weapon7",
+    "weapon8",
+    "weapon9",
+    "weapon10",
+    "weapon11",
+    "weapon12",
+    "weapon13",
+    "weapon14",
+    "weapon15",
+    "weapon16",
+    "weapon17",
+    "weapon18",
+    "weapon19",
+    "weapon20",
+  ];
+});
