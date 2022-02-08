6a73172f9c8a1a7afd6c1e7a2c26464b6f5783ef
PJ Jones <pj.jones4000.com>
2022-02-07 18:27:29 -0600

    i give up

--- script.js
+++ script.js
@@ -1,26 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-  var div = document.createElement("div");
-  document.body.appendChild(div);
-
-  function run() {
-    console.log(
-      `Accusation ${i}:I accuse ${friends[i % friends.length]}, with the ${
-        weapons[i % weapons.length]
-      } in the ${locations[i % locations.length]}.`
-    );
-  };
-
-  for (var i = 1; i <= 100; i++) {
-    var h3 = document.createElement("h3");
-    h3.addEventListener("click", run);
-    h3.textContent = "Accusation " + i;
-    h3.id = "acc" + i;
-
-    
-
-    div.appendChild(h3);
-  }
-
   var friends = ["friend1", "friend2", "friend3", "friend4", "friend5"];
   var locations = [
     "location1",
@@ -56,4 +34,50 @@ document.addEventListener("DOMContentLoaded", function () {
     "weapon19",
     "weapon20",
   ];
+
+  var div = document.createElement("div");
+  document.body.appendChild(div);
+
+  function displayAcc() {
+    for (var i = 1; i <= 100; i++) {
+      console.log(
+        `Accusation ${i}: I accuse ${friends[i % friends.length]}, with the ${
+          weapons[i % weapons.length]
+        } in the ${locations[i % locations.length]}.`
+      );
+
+      var h3 = document.createElement("h3");
+      h3.textContent = "Accusation " + i;
+      h3.id = "acc" + i;
+      h3.addEventListener("click", );
+      
+      div.appendChild(h3);
+    }
+    
+  }
+  var myFunc = displayAcc();
+
+  // function run() {
+  //   console.log(
+  //     `Accusation ${i}: I accuse ${friends[i % friends.length]}, with the ${
+  //       weapons[i % weapons.length]
+  //     } in the ${locations[i % locations.length]}.`
+  //   );
+  // };
+  // function makeFunc() {
+  //   for (var i = 1; i <= 100; i++) {
+  //     var h3 = document.createElement("h3");
+  //     h3.textContent = "Accusation " + i;
+  //     h3.id = "acc" + i;
+  //     // h3.addEventListener("click", displayAcc);
+  //     div.appendChild(h3);
 });
+
+// for (var i = 1; i <= 100; i++) {
+//   var h3 = document.createElement("h3");
+//   h3.addEventListener("click", myFunc);
+//   h3.textContent = "Accusation " + i;
+//   h3.id = "acc" + i;
+
+//   div.appendChild(h3);
+// }
