5896203ee9ac1df6d58d2dec3890119ce350ff88
PJ Jones <pj.jones4000.com>
2022-02-07 21:52:10 -0600

    I think it's done

--- script.js
+++ script.js
@@ -1,58 +1,63 @@
 document.addEventListener("DOMContentLoaded", function () {
-  var friends = ["friend1", "friend2", "friend3", "friend4", "friend5"];
+  var friends = ["Alyssa", "Larry", "Amanda", "Thomas", "Phillip"];
   var locations = [
-    "location1",
-    "location2",
-    "location3",
-    "location4",
-    "location5",
-    "location6",
-    "location7",
-    "location8",
-    "location9",
-    "location10",
+    "kitchen",
+    "dining room",
+    "bar",
+    "basement",
+    "attic",
+    "swimming pool",
+    "billiard room",
+    "laundry room",
+    "bedroom",
+    "hot tub roon",
   ];
   var weapons = [
-    "weapon1",
-    "weapon2",
-    "weapon3",
-    "weapon4",
-    "weapon5",
-    "weapon6",
-    "weapon7",
-    "weapon8",
-    "weapon9",
-    "weapon10",
-    "weapon11",
-    "weapon12",
-    "weapon13",
-    "weapon14",
-    "weapon15",
-    "weapon16",
-    "weapon17",
-    "weapon18",
-    "weapon19",
-    "weapon20",
+    "rope",
+    "knife",
+    "silly string",
+    "cat litter",
+    "poison frog",
+    "coffee pot",
+    "pencil",
+    "sword",
+    "crossbow",
+    "dumbell",
+    "monkey wrench",
+    "baseball bat",
+    "soccer ball",
+    "lead pipe",
+    "bike lock",
+    "water gun",
+    "beer bottle",
+    "hot pocket",
+    "slim jim",
+    "ninja star",
   ];
 
   var div = document.createElement("div");
   document.body.appendChild(div);
 
-  for (var i = 1; i <= 100; i++) {
+  function outsideFunc() {
     var h3 = document.createElement("h3");
     h3.textContent = "Accusation " + i;
-    h3.id = "acc" + i;
-    h3.addEventListener("click", run);
+    h3.id = i;
+    h3.addEventListener("click", alertFunc);
     div.appendChild(h3);
-
-    function returnRun() {
-      function run() {
-        console.log(
-          `Accusation ${i}: I accuse ${friends[i % friends.length]}, with the ${
-            weapons[i % weapons.length]
-          } in the ${locations[i % locations.length]}.`
-        );
-      }
+    function alertFunc() {
+      alert(
+        `Accusation ${event.target.id}: I accuse ${friends[event.target.id % friends.length]}, with the ${
+          weapons[event.target.id % weapons.length]
+        } in the ${locations[event.target.id % locations.length]}.`
+      );
     }
+
+    return alertFunc;
+  }
+
+  
+
+  for (var i = 1; i <= 100; i++) {
+    outsideFunc(i);
   }
 });
