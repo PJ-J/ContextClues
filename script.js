76cee6487aea8a9316c2ae60ce038a7f94e9da5f
PJ Jones <pj.jones4000.com>
2022-02-07 18:51:15 -0600

    trying to make a function that returns a function that I can use as event listener

--- script.js
+++ script.js
@@ -38,46 +38,21 @@ document.addEventListener("DOMContentLoaded", function () {
   var div = document.createElement("div");
   document.body.appendChild(div);
 
-  function displayAcc() {
-    for (var i = 1; i <= 100; i++) {
-      console.log(
-        `Accusation ${i}: I accuse ${friends[i % friends.length]}, with the ${
-          weapons[i % weapons.length]
-        } in the ${locations[i % locations.length]}.`
-      );
+  for (var i = 1; i <= 100; i++) {
+    var h3 = document.createElement("h3");
+    h3.textContent = "Accusation " + i;
+    h3.id = "acc" + i;
+    h3.addEventListener("click", run);
+    div.appendChild(h3);
 
-      var h3 = document.createElement("h3");
-      h3.textContent = "Accusation " + i;
-      h3.id = "acc" + i;
-      h3.addEventListener("click", );
-      
-      div.appendChild(h3);
+    function returnRun() {
+      function run() {
+        console.log(
+          `Accusation ${i}: I accuse ${friends[i % friends.length]}, with the ${
+            weapons[i % weapons.length]
+          } in the ${locations[i % locations.length]}.`
+        );
+      }
     }
-    
   }
-  var myFunc = displayAcc();
-
-  // function run() {
-  //   console.log(
-  //     `Accusation ${i}: I accuse ${friends[i % friends.length]}, with the ${
-  //       weapons[i % weapons.length]
-  //     } in the ${locations[i % locations.length]}.`
-  //   );
-  // };
-  // function makeFunc() {
-  //   for (var i = 1; i <= 100; i++) {
-  //     var h3 = document.createElement("h3");
-  //     h3.textContent = "Accusation " + i;
-  //     h3.id = "acc" + i;
-  //     // h3.addEventListener("click", displayAcc);
-  //     div.appendChild(h3);
 });
-
-// for (var i = 1; i <= 100; i++) {
-//   var h3 = document.createElement("h3");
-//   h3.addEventListener("click", myFunc);
-//   h3.textContent = "Accusation " + i;
-//   h3.id = "acc" + i;
-
-//   div.appendChild(h3);
-// }
